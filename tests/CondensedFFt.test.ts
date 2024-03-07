ab5e3ee72d275904cc6cdd3eceae7b0024618c44
Sushma Rana <sushma.rana@mauryaprojects.com>
2024-03-07 12:41:37 +0530

    CondensedFFt - test

--- tests/CondensedFFt.test.ts
+++ tests/CondensedFFt.test.ts
@@ -62,6 +62,65 @@ describe('unit | CondensedFFt', () => {
         condensedFftTotal: 1,
       },
     ],
+    [
+      'without part payload',
+      '13202000170007004a0003',
+      null,
+      {
+        condensedFft: [
+          {
+            data: '13202000170007004a0003',
+            axis: '1',
+            condensedFft: {
+              type: 'Peak Energy Values',
+              values: {
+                '10-20Hz': '0.00000',
+                '21-40Hz': '0.00000',
+                '41-55Hz': '0.00000',
+                '56-70Hz': '0.00000',
+              },
+            },
+          },
+        ],
+        condensedFftTotal: 1,
+      },
+    ],
+    [
+      'with part payload',
+      '13202000170007004a0003',
+      '1420300004000100010000',
+      {
+        condensedFft: [
+          {
+            data: '13202000170007004a0003',
+            axis: '1',
+            condensedFft: {
+              type: 'Peak Energy Values',
+              values: {
+                '10-20Hz': '0.00000',
+                '21-40Hz': '0.00000',
+                '41-55Hz': '0.00000',
+                '56-70Hz': '0.00000',
+              },
+            },
+          },
+          {
+            data: '1420300004000100010000',
+            axis: '1',
+            condensedFft: {
+              type: 'Peak Energy Values',
+              values: {
+                '71-110Hz': '0.00000',
+                '111-130Hz': '0.00000',
+                '131-230Hz': '0.00000',
+                '231Hz+': '0.00000',
+              },
+            },
+          },
+        ],
+        condensedFftTotal: 1,
+      },
+    ],
   ])(
     'decodes an condensedfft %s event',
     (description, payload, payloadTwo, result) => {
